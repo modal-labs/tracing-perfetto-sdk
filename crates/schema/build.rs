9857fcb97bafce58940ac84ea05b921b19fc958c
David Flemström <dflemstr@users.noreply.github.com>
2024-11-18 13:15:42 +0100

    schema: Conditionally enable serde support (#13)

--- crates/schema/build.rs
+++ crates/schema/build.rs
@@ -10,6 +10,31 @@ fn main() -> anyhow::Result<()> {
         println!("cargo:rerun-if-changed={}", proto_file.display());
     }
 
+    compile(proto_files, &[proto_root])?;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "serde"))]
+// Compile for `serde`: emit a file descriptor set, substitute `prost_types` for
+// `pbjson_types`, and use `pbjson_build` to generate serde traits.
+fn compile(
+    proto_files: &[impl AsRef<path::Path>],
+    includes: &[impl AsRef<path::Path>],
+) -> anyhow::Result<()> {
+    prost_build::Config::new()
+        .bytes(["."])
+        .compile_protos(proto_files, includes)?;
+    Ok(())
+}
+
+#[cfg(feature = "serde")]
+// Compile for `serde`: emit a file descriptor set, substitute `prost_types` for
+// `pbjson_types`, and use `pbjson_build` to generate serde traits.
+fn compile(
+    proto_files: &[impl AsRef<path::Path>],
+    includes: &[impl AsRef<path::Path>],
+) -> anyhow::Result<()> {
     let descriptor_path = path::PathBuf::from(env::var("OUT_DIR")?).join("proto_descriptor.bin");
 
     prost_build::Config::new()
@@ -17,12 +42,11 @@ fn main() -> anyhow::Result<()> {
         .file_descriptor_set_path(&descriptor_path)
         .compile_well_known_types()
         .extern_path(".google.protobuf", "::pbjson_types")
-        .compile_protos(proto_files, &[proto_root])?;
+        .compile_protos(proto_files, includes)?;
 
     let descriptor_set = std::fs::read(descriptor_path)?;
     pbjson_build::Builder::new()
         .register_descriptors(&descriptor_set)?
         .build(&[".perfetto"])?;
-
     Ok(())
 }
